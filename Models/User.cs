409af587e7d4823b59f2d072840a9a9c183e4889
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-10-23 13:36:48 -0300

    criacao do projeto

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Models
+{
+
+    [Table("[User]")]
+    public class User
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string PasswordHash { get; set; }
+
+        public required string Bio { get; set; }
+
+        public required string Image { get; set; }
+
+        public required string Slug { get; set; }
+    }
+}
